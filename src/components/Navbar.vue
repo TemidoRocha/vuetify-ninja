1e18e869e2950c9a2b28571fc5d15aafcf205f0e
TemidoRocha <manueltemidorocha@gmail.com>
2020-11-08 08:54:09 +0000

    navbar, nav-icon, toollbar-title. mavigation-drawer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer app v-model="drawer" class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+  }),
+};
+</script>
+
+<style></style>
